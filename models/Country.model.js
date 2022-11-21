826534d6f47f9fd106b72a863b1a308ae3ed8956
Ropinmar <rodrigo.pinzon94@gmail.com>
2022-11-21 07:55:34 -0500

    modelos y subi las fotos en formato jpeg

---
+++ models/Country.model.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const countrySchema = new Schema(
+  {
+    countryName: {
+      type: String,
+      required: [true, "countryName is required."],
+      trim: true,
+    },
+    info: {
+      type: String,
+      required: [false],
+      trim: true,
+    },
+    flagImg: {
+      type: String,
+      required: [false],
+    }
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Country = model("Country", countrySchema);
+
+module.exports = Country;
