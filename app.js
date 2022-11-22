aeb5003076ae3951ca67b08e69a4921b9fafbda5
OmarMeza15 <omarmezajarquin@gmail.com>
2022-11-22 17:40:23 +0100

    Added two more routes

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ app.use("/", indexRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const postRoutes = require("./routes/post.routes");
+app.use("/", postRoutes);
+
+const startPagesRoutes = require("./routes/startPages.routes");
+app.use("/", startPagesRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
