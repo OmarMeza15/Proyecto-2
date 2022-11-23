a77032465671ccd07ed64cbd7f1a72f6c6382d3f
OmarMeza15 <omarmezajarquin@gmail.com>
2022-11-23 03:55:28 +0100

    started working on showing the posts on profile

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,9 +2,13 @@ const Country = require("../models/Country.model");
 
 const router = require("express").Router();
 
+const Post = require("../models/Post.model");
+
 //GET para ingresar /user/profile
 router.get("/profile", (req, res) => {
-    res.render("user/profile", { currentUser: req.session.currentUser });
+    Post.find()
+        .then((PostDB) => res.render("user/profile", { currentUser: req.session.currentUser, post: PostDB }))
+        .catch((err) => console.log(err));
 });
 
 router.get("/newCountry", (req, res, next) => {
@@ -15,8 +19,10 @@ router.post("/newCountry", (req, res, next) => {
     const { countryName, info, flagImg } = req.body;
 
     Country.create({ countryName, info, flagImg })
-    .then(() => console.log("success") )
+    .then(() => res.render("user/newCountry", { currentUser: req.session.currentUser }))
     .catch((err) => console.log(err));
 })
+
+
 
 module.exports = router;
